8ff18094978b42353653efdf282a5cbc95e19569
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-06-02 01:42:50 -0300

    cont. refactoring old code

--- lib/decorators/path.ts
+++ lib/decorators/path.ts
@@ -1,8 +1,9 @@
-import { Routes } from '../routes';
+import { RouteLoader } from '../loaders/route.loader';
+
 
 export function path(path: string) {
   return function (target: Object) {
     const className: string = target.toString().match(/function ([^(]*)/)[1];
-    Routes.addRouterConfig(className, path);
+    RouteLoader.addRouterConfig(className, path);
   };
 }
---
+++ lib/utils/path.utils.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+import { OrcheNames } from '../constants/orchenames';
+import * as appRootPath from 'app-root-path';
+
+
+export class PathUtils {
+
+  static appRoot: string = appRootPath.path;
+  static appDirName: string = path.basename(appRootPath.path);
+  static localConfigFile: string = path.resolve(appRootPath.path, './'
+    .concat(OrcheNames.configFile));
+
+  /**
+   * Makes the url sanitation, in other words, this function includes the beginning slash
+   * and removes the ending slash, if not existent and existent respectively.
+   */
+  static urlSanitation(url: string): string {
+    let finalUrl = url;
+
+    if (!finalUrl || finalUrl.trim().length === 0) {
+      return '/';
+    } else if (url.length === 1 && url === '/') {
+      return finalUrl;
+    }
+
+    // Beginning url sanitation
+    if (!url.match(/^\//)) {
+      finalUrl = '/' + url;
+    }
+    // Ending url sanitation
+    if (url.match(/\/$/)) {
+      finalUrl = url.substr(0, (url.length - 1));
+    }
+
+    return finalUrl;
+  }
+
+}
