899fabe72524ec8945d222919bab2f735a813fae
Mauricio Gemelli Vigolo <mauriciogv@sanepar.com.br>
2017-06-07 18:04:09 -0300

    cont. tests in package utils

--- tests/utils/package.utils.spec.ts
+++ tests/utils/package.utils.spec.ts
@@ -36,5 +36,44 @@ describe('PackageUtils', () => {
           found ${version}`);
       }
     });
+
+    it('should return a dependency version from devDependencies', () => {
+      const version = pkgUtils.getDependencyVersion('ts-node');
+
+      if (!version) {
+        throw new Error(`Expected to find the ts-node version in the devDependencies keys but 
+          found ${version}`);
+      }
+    });
   });
+
+  describe('#checkDependencyVersion', () => {
+    it('should throw error if the dependency was not found', () => {
+      let result;
+      try {
+        result = pkgUtils.checkDependencyVersion('not-a-dependency', '1.0.0');
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the dependency was not found, but returned
+        ${result}`);
+    });
+
+    it('should return "equal" if the version is the same', () => {
+      let result;
+      try {
+        result = pkgUtils.checkDependencyVersion('file-matcher', '1.1.0');
+      } catch (error) {
+        return;
+      }
+
+      if (result ===) {
+        
+      }
+      throw new Error(`Should throw an error if the dependency was not found, but returned
+        ${result}`);
+    });
+  });
+
 });
