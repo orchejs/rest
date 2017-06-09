f770fb9303e8a917a7e2ea83f8ff2dca7b965d2d
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-06-09 14:30:12 -0300

    test(core): added tests to check depend. version compab.

--- tests/utils/package.utils.spec.ts
+++ tests/utils/package.utils.spec.ts
@@ -60,7 +60,67 @@ describe('PackageUtils', () => {
         ${result}`);
     });
 
-    it('should return "equal" if the version is the same', () => {
+    it('should throw error if the version to check is undefined', () => {
+      let result;
+      try {
+        result = pkgUtils.checkDependencyVersion('file-matcher', undefined);
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the version to check is undefined, but returned
+        ${result}`);
+    });
+
+    it('should throw error if the version to check is null', () => {
+      let result;
+      try {
+        result = pkgUtils.checkDependencyVersion('file-matcher', null);
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the version to check is null, but returned
+        ${result}`);
+    });
+
+    it('should throw error if the version to check is blank', () => {
+      let result;
+      try {
+        result = pkgUtils.checkDependencyVersion('file-matcher', '');
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the version to check is blank, but returned
+        ${result}`);
+    });
+
+    it('should throw error if the version to check contains invalid chars', () => {
+      let result;
+      try {
+        result = pkgUtils.checkDependencyVersion('file-matcher', 'asf.sadas');
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the version to check contains invalid chars, but 
+       returned ${result}`);
+    });
+
+    it('should throw error if the version to check does not follow the semantic versioning', () => {
+      let result;
+      try {
+        result = pkgUtils.checkDependencyVersion('file-matcher', 'asf.2das');
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the version does not follow the SEMVER, but returned
+        ${result}`);
+    });
+
+    it('should return "eq" if the version is the same', () => {
       let result;
       try {
         result = pkgUtils.checkDependencyVersion('file-matcher', '1.1.0');
@@ -68,13 +128,13 @@ describe('PackageUtils', () => {
         return;
       }
 
-      if (result !== 'equal') {
+      if (result !== 'eq') {
         throw new Error(`Should throw an error if the result is different of equal. The result 
         value is ${result}`);
       }
     });
 
-    it('should return "greater" if the version param is higher than the dependency', () => {
+    it('should return "gt" if the version param is higher than the dependency', () => {
       let result;
       try {
         result = pkgUtils.checkDependencyVersion('file-matcher', '1.2.1');
@@ -82,13 +142,13 @@ describe('PackageUtils', () => {
         return;
       }
 
-      if (result !== 'greater') {
+      if (result !== 'gt') {
         throw new Error(`Should throw an error if the result is different of greater. The result 
         value is ${result}`);
       }
     });
 
-    it('should return "less" if the version param is lesser than the dependency', () => {
+    it('should return "lt" if the version param is lesser than the dependency', () => {
       let result;
       try {
         result = pkgUtils.checkDependencyVersion('file-matcher', '1.0.1');
@@ -96,10 +156,76 @@ describe('PackageUtils', () => {
         return;
       }
 
-      if (result !== 'less') {
+      if (result !== 'lt') {
         throw new Error(`Should throw an error if the result is different of less. The result 
         value is ${result}`);
       }
     });
   });
+
+  describe('#isDependencyVersionCompatible', () => {
+    it('should throw error if the dependency was not found', () => {
+      let result;
+      try {
+        result = pkgUtils.isDependencyVersionCompatible('not-a-dependency', '1.0.0', '1.0.0');
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the dependency was not found, but returned
+        ${result}`);
+    });
+
+    it('should throw error if the version from or to, to check is not right', () => {
+      let result;
+      try {
+        result = pkgUtils.isDependencyVersionCompatible('file-matcher', undefined, '1;as');
+      } catch (error) {
+        return;
+      }
+
+      throw new Error(`Should throw an error if the version from or to, but returned ${result}`);
+    });
+
+    it('should return false if fromVersion is higher than the dependency in package.json', () => {
+      let result;
+      try {
+        result = pkgUtils.isDependencyVersionCompatible('file-matcher', '2.0.0', '2.0.0');
+      } catch (error) {
+        throw new Error(`Should not throw an exception if the params are right`);
+      }
+
+      if (result) {
+        throw new Error(`Should return false if the fromVersion is higher, but returned ${result}`);
+      }
+    });
+
+    it('should return false if toVersion is lesser than the dependency in package.json', () => {
+      let result;
+      try {
+        result = pkgUtils.isDependencyVersionCompatible('file-matcher', '1.0.0', '1.0.1');
+      } catch (error) {
+        throw new Error(`Should not throw an exception if the params are right`);
+      }
+
+      if (result) {
+        throw new Error(`Should return false if the toVersion is lesser, but returned ${result}`);
+      }
+    });
+
+    it(`should return true if fromVersion & toVersion are lesser and greater`, () => {
+      let result;
+      try {
+        result = pkgUtils.isDependencyVersionCompatible('file-matcher', '1.0.0', '2.0.0');
+      } catch (error) {
+        throw new Error(`Should not throw an exception if the params are right`);
+      }
+
+      if (!result) {
+        throw new Error(`Should return true if fromVersion & toVersion are lesser and greater, 
+        but returned ${result}`);
+      }
+    });
+  });
+
 });
