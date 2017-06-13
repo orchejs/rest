5f9a31747b23d3bb5dac9a416a5bc7a3bbe4c471
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-06-12 23:58:07 -0300

    refactor(core): router name change

--- lib/decorators/http.ts
+++ lib/decorators/http.ts
@@ -1,61 +1,61 @@
 import { HttpRequestMethod } from '../constants/http-request-method';
-import { RouteLoader } from '../loaders/route.loader';
+import { RouterLoader } from '../loaders/router.loader';
 import { CorsOptions } from '../interfaces/cors-options';
 
 
 export function all(path: string = '', corsOptions?: CorsOptions, preflight?: boolean) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey,
-                             HttpRequestMethod.All);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey,
+                              HttpRequestMethod.All);
   };
 }
 
 export function get(path: string = '', corsOptions?: CorsOptions, preflight?: boolean) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
     const method: Function = descriptor.value;
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
-                             HttpRequestMethod.Get);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
+                              HttpRequestMethod.Get);
   };
 }
 
 export function post(path: string = '', corsOptions?: CorsOptions, preflight?: boolean) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
-                             HttpRequestMethod.Post);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
+                              HttpRequestMethod.Post);
   };
 }
 
 export function put(path: string = '', corsOptions?: CorsOptions, preflight?: boolean) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
-                             HttpRequestMethod.Put);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
+                              HttpRequestMethod.Put);
   };
 }
 
 export function del(path: string = '', corsOptions?: CorsOptions, preflight?: boolean) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
-                             HttpRequestMethod.Delete);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
+                              HttpRequestMethod.Delete);
   };
 }
 
 export function patch(path: string = '', corsOptions?: CorsOptions, preflight?: boolean) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
-                             HttpRequestMethod.Patch);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
+                              HttpRequestMethod.Patch);
   };
 }
 
 export function options(path: string = '', corsOptions?: CorsOptions) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey,
-                             HttpRequestMethod.Options);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey,
+                              HttpRequestMethod.Options);
   };
 }
 
 export function head(path: string = '', corsOptions?: CorsOptions, preflight?: boolean) {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
-    RouteLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
-                             HttpRequestMethod.Head);
+    RouterLoader.addRouteUnit(path, descriptor.value.bind(target), propertyKey, 
+                              HttpRequestMethod.Head);
   };
 }
