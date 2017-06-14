2a7ab5f5b207781397c1994182ae8d72f6944173
Mauricio Vigolo <mauriciovigolo@gmail.com>
2017-06-14 00:13:26 -0300

    fix(core): router name change

--- lib/decorators/path.ts
+++ lib/decorators/path.ts
@@ -1,9 +1,9 @@
-import { RouteLoader } from '../loaders/route.loader';
+import { RouterLoader } from '../loaders/router.loader';
 
 
 export function path(path: string) {
   return function (target: Object) {
     const className: string = target.toString().match(/function ([^(]*)/)[1];
-    RouteLoader.addRouterConfig(className, path);
+    RouterLoader.addRouterConfig(className, path);
   };
 }
