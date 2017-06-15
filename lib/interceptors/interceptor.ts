08eef90d7a6438bae0dd5fa49e5b841813353026
Mauricio Vigolo <mauriciovigolo@gmail.com>
2017-06-15 00:14:21 -0300

    feat(core): express interceptor implementation

--- lib/interceptors/interceptor.ts
+++ lib/interceptors/interceptor.ts
@@ -2,8 +2,11 @@ import { InterceptorType } from '../constants/interceptor-type';
 import { InterceptorConfig } from '../interfaces/interceptor-config';
 
 export abstract class Interceptor {
+
   protected abstract loadPreProcessors(app: any): Promise<any>;
+
   protected abstract loadPostProcessors(app: any): Promise<any>;
-  protected abstract loadInterceptorUnit(app: any, interceptorType: InterceptorType):
+
+  protected abstract loadInterceptorUnit(app: any, interceptorType: InterceptorType): 
     InterceptorConfig[];
 }
