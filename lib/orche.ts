f5298f3b037fceaf9f9abe0bb1c533f091d21527
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-06-21 07:52:11 -0300

    feat(main): started to implement main orche file

--- lib/orche.ts
+++ lib/orche.ts
@@ -1,3 +1,23 @@
-export default class Orche {
+import { EventEmitter } from 'events';
+
+import { OrcheConfig } from './interfaces/orche-config';
+import { DecoratorLoader } from './loaders/decorator.loader';
+
+export class Orche extends EventEmitter {
+
+  constructor() {
+    super();
+  }
+
+  init(config: OrcheConfig): void {
+    const decoratorLoader = new DecoratorLoader();
+    decoratorLoader.loadDecorators()
+      .then(() => {
+
+      })
+      .catch((e) => {
+
+      });
+  }
 
 }
