13f4b5e3f4cd33d92f92e3220a6fbb3bd3c8a4bf
Mauricio Vigolo <mauriciovigolo@gmail.com>
2017-06-27 06:42:16 -0300

    test(engine): express engine tests impl

--- tests/engines/express.engine.spec.ts
+++ tests/engines/express.engine.spec.ts
@@ -1,27 +1,16 @@
 import { ExpressEngine } from '../../lib/engines/express.engine';
 
-/*
-describe('PackageUtils', () => {
+describe('ExpressEngine', () => {
   let exEngine: ExpressEngine;
 
   before(() => {
-    exEngine = new ExpressEngine();
-  });
-
-  describe('#loadOrcheConfig', () => {
-    it('Should return false if version is not compatible', () => {
-
-    });
+    
   });
 
   describe('#loadServer', () => {
-    it('should return the app description from package.json', () => {
-      const description = exEngine.getDescrition();
-
-      if (!description) {
-        throw new Error(`Expected a description for the app, but was ${description}`);
-      }
+    it('Should initialize the express app with the default options', async () => {
+      exEngine = new ExpressEngine();
+      const server = await exEngine.loadServer();
     });
   });
 });
-*/
