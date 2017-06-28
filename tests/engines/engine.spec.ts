15dc672c03c6e008b1d8e1f119e71ed5e1c5e558
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-06-28 01:12:29 -0300

    test(engine): cont engine tests implementation

--- tests/engines/engine.spec.ts
+++ tests/engines/engine.spec.ts
@@ -8,64 +8,6 @@ import { CompatVersions } from '../../lib/interfaces/compat-versions';
 import { PathUtils } from '../../lib/utils/path.utils';
 
 describe('Engine', () => {
-  /**
-   * Engine with a not supported version.
-   */
-  class IncompatibleEngine extends Engine {
-    constructor() {
-      super(
-        {
-          dependency: 'express',
-          from: '1.0.0',
-          to: '1.1.0',
-        },
-      );
-    }
-
-    public loadServer(): Promise<any> {
-      throw new Error('Not implemented');
-    }
-    protected setupSettings(): void {
-      throw new Error('Not implemented');
-    }
-    protected setupExtensions(): void {
-      throw new Error('Not implemented');
-    }
-  }
-
-  /**
-   * Version is compatible. It will test all engine 
-   * generic methods.
-   */
-  class SpecEngine extends Engine {
-    constructor() {
-      super(
-        {
-          dependency: 'express',
-          from: '1.0.0',
-          to: '5.0.0',
-        },
-        {
-          path: '/myapp',
-          initMessage: 'App initialized',
-          extensions: [],
-          settings: {},
-          corsConfig: {
-            origin: '*',
-          },
-        });
-    }
-
-    public loadServer(): Promise<any> {
-      throw new Error('Not implemented');
-    }
-    protected setupSettings(): void {
-      throw new Error('Not implemented');
-    }
-    protected setupExtensions(): void {
-      throw new Error('Not implemented');
-    }
-  }
 
   beforeEach(() => {
     process.env.ORCHE_CONFIG = '';
@@ -90,14 +32,14 @@ describe('Engine', () => {
 
     it('Should load orche configs from env.ORCHE_CONFIG', () => {
       process.env.ORCHE_CONFIG = PathUtils.localConfigFile;
-      
+
       let engine: SpecEngine;
       try {
         engine = new SpecEngine();
         expect(engine).to.not.null;
       } catch (e) {
         expect(e).to.null;
-      }      
+      }
     });
 
     it('Should load orche configs, following the hierarchy', () => {
@@ -112,38 +54,97 @@ describe('Engine', () => {
 
     it('Should throw error if the env.ORCHE_CONFIG file is wrong', () => {
       process.env.ORCHE_CONFIG = path.resolve(PathUtils.appRoot, 'LICENSE');
-      
+
       let engine: SpecEngine;
       try {
         engine = new SpecEngine();
         expect(engine).to.null;
       } catch (e) {
         expect(e).to.not.null;
-      }      
+      }
     });
 
     it('Should throw error if the env.ORCHE_CONFIG file was informed but not found', () => {
       process.env.ORCHE_CONFIG = path.resolve(PathUtils.appRoot, 'not-a-existent-file.json');
-      
+
       let engine: SpecEngine;
       try {
         engine = new SpecEngine();
         expect(engine).to.null;
       } catch (e) {
         expect(e).to.not.null;
-      }      
+      }
     });
 
     it('Should throw error if the local ORCHE_CONFIG file is wrong', () => {
       PathUtils.localConfigFile = path.resolve(PathUtils.appRoot, 'LICENSE');
-      
+
       let engine: SpecEngine;
       try {
         engine = new SpecEngine();
         expect(engine).to.null;
       } catch (e) {
         expect(e).to.not.null;
-      }      
-    });    
+      }
+    });
   });
 });
+
+/**
+ * Engine with a not supported version.
+ */
+class IncompatibleEngine extends Engine {
+  constructor() {
+    super(
+      {
+        dependency: 'express',
+        from: '1.0.0',
+        to: '1.1.0',
+      },
+    );
+  }
+
+  public loadServer(): Promise<any> {
+    throw new Error('Not implemented. Doen\'t matter for these tests');
+  }
+  protected setupSettings(): void {
+    throw new Error('Not implemented. Doen\'t matter for these tests');
+  }
+  protected setupExtensions(): void {
+    throw new Error('Not implemented. Doen\'t matter for these tests');
+  }
+}
+
+/**
+ * Version is compatible. It will test all engine 
+ * generic methods.
+ */
+class SpecEngine extends Engine {
+  constructor() {
+    super(
+      {
+        dependency: 'express',
+        from: '1.0.0',
+        to: '5.0.0',
+      },
+      {
+        path: '/myapp',
+        initMessage: 'App initialized',
+        extensions: [],
+        settings: {},
+        corsConfig: {
+          origin: '*',
+        },
+      });
+  }
+
+  public loadServer(): Promise<any> {
+    throw new Error('Not implemented. Doen\'t matter for these tests');
+  }
+  protected setupSettings(): void {
+    throw new Error('Not implemented. Doen\'t matter for these tests');
+  }
+  protected setupExtensions(): void {
+    throw new Error('Not implemented. Doen\'t matter for these tests');
+  }
+}
