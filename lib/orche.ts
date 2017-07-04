afe4bba18446b3d56e9edba104447c95e890a975
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-07-04 18:06:34 -0300

    feat(core): orche decorator loader and engine initialization

--- lib/orche.ts
+++ lib/orche.ts
@@ -1,23 +1,66 @@
-import { EventEmitter } from 'events';
-
 import { OrcheConfig } from './interfaces/orche-config';
+import { OrcheResult } from './interfaces/orche-result';
 import { DecoratorLoader } from './loaders/decorator.loader';
+import { OrcheEngines } from './constants/orche-engines';
+import { ExpressEngine } from './engines/express.engine';
 
-export class Orche extends EventEmitter {
+export class Orche {
 
-  constructor() {
-    super();
+  init(config: OrcheConfig): Promise<OrcheResult> {
+    return new Promise(async (resolve, reject) => {
+
+      this.loadDecorators(reject);
+
+      const engine = this.engineInitialization(config, reject);
+      try {
+        engine.loadServer();
+      } catch (error) {
+        // TODO
+        reject();
+      }
+
+      resolve();
+    });
   }
 
-  init(config: OrcheConfig): void {
+  private async loadDecorators(reject): string[] {
     const decoratorLoader = new DecoratorLoader();
-    decoratorLoader.loadDecorators()
-      .then(() => {
 
-      })
-      .catch((e) => {
+    let files: string [];
+    try {
+      files = await decoratorLoader.loadDecorators();
+    } catch (error) {
+      // TODO
+      reject();
+    }
 
-      });
+    return files;
+  }
+
+  private engineInitialization(config: OrcheConfig, reject: any) {
+    let engine;
+
+    try {
+      switch (config.apiEngine) {
+        case OrcheEngines.ExpressJS:
+          engine = new ExpressEngine(config);
+          break;
+        case OrcheEngines.Hapi:
+          break;
+        case OrcheEngines.Koa:
+          break;
+        case OrcheEngines.Restify:
+          break;
+        default:
+          engine = new ExpressEngine(config);
+          break;
+      }
+    } catch (error) {
+      // TODO
+      reject();
+    }
+
+    return engine;
   }
 
 }
