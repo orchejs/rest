b2c837f6f94bb268cda90ec6d569645dc28a9610
Mauricio Gemelli Vigolo <mauriciovigolo@Mauricios-MacBook-Air.local>
2017-07-14 07:32:26 -0300

    test(orche): writing tests for the main execution

--- tests/orche.spec.ts
+++ tests/orche.spec.ts
@@ -1,19 +1,35 @@
 import { expect } from 'chai';
+import { path, get, Orche, OrcheConfig, OrcheEngines } from '../../index';
 
-describe('orche', () => {
+describe('Orche Main Class', () => {
 
-  beforeEach(() => {
+  before(async () => {
+    let orche = new Orche();
+
+    const config: OrcheConfig = {
+      path: '/orche-test',
+      apiEngine: OrcheEngines.ExpressJS,
+      port: 8888
+    };
+
+      let result = await orche.init(config)
+      console.log('ok', result);
   });
 
-  after(() => {
-  });
-
-  describe('#constructor', () => {
-  });
+  describe('#init', (() => {
+    it('Should initialize routes', () => {
+      expect(true).to.be.true;
+    });
+  }));
 
 });
 
+@path('/utilities')
+class Utilities {
 
-class OrcheTestEndpoints {
+  @get()
+  justPing() {
+    return { msg: 'ping' };
+  }
 
 }
