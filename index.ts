bf73cada3859e0ff3c7ff9c8f85c24fc67013146
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-07-23 22:51:51 -0300

    feat: added new exports

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ export * from './lib/constants/interceptor-type';
 export * from './lib/constants/http-request-method';
 export * from './lib/constants/http-response-code';
 export * from './lib/constants/orche-engines';
+export * from './lib/constants/mimetype';
 
 // interfaces
 export * from './lib/interfaces/orche-config';
