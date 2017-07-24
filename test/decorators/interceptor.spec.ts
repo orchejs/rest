29650cb75ef75e6d118ccc886131b1e04a5a2b03
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-07-23 22:53:15 -0300

    test(intercetor): added new file to test interceptors.

---
+++ test/decorators/interceptor.spec.ts
@@ -0,0 +1,18 @@
+import { hostname } from 'os';
+import { expect } from 'chai';
+import { Request } from 'express';
+import { json } from 'body-parser';
+import { RequestHelper } from '../helpers/request.helper';
+
+import {
+  Orche, OrcheConfig, OrcheEngines, OrcheResult,
+} from '../../';
+
+
+describe('Interceptor Decorators Tests', () => {
+
+  it('Should interceptor /orche-test/utilities', () => {
+
+  });
+
+});
