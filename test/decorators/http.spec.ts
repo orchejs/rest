172e1efab3e855cac86e9fdaae9390d7b12a0c8d
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-07-25 18:01:12 -0300

    test(decorator): server init/shutdown and interceptor tests implementation

--- test/decorators/http.spec.ts
+++ test/decorators/http.spec.ts
@@ -14,7 +14,7 @@ describe('HTTP Decorators Tests', () => {
     const orche = new Orche();
 
     const config: OrcheConfig = {
-      path: '/orche-decorators',
+      path: '/orche',
       apiEngine: OrcheEngines.ExpressJS,
       port: 8888,
       extensions: [
@@ -24,45 +24,53 @@ describe('HTTP Decorators Tests', () => {
     result = await orche.init(config);
   });
 
+  after((done) => {
+    if (result.server) {
+      result.server.close(() => {
+        done();
+      });
+    }
+  });
+
   it('Should initialize routes', () => {
     expect(result.stats.routerStats.loadedRoutes.length).to.be.gt(0);
   });
 
-  it('Should make an http GET to /orche-decorators/utilities and receive \'ping\'', async () => {
-    const result: string = await RequestHelper.get('/orche-decorators/utilities');
+  it('Should make an http GET to /orche/utilities and receive \'ping\'', async () => {
+    const result: string = await RequestHelper.get('/orche/utilities');
     expect(result).to.be.equal('{"msg":"ping"}');
   });
 
-  it('Should make an http POST to /orche-decorators/utilities and receive \'ping {ip}\'', async () => {
-    const result: string = await RequestHelper.post('/orche-decorators/utilities',
+  it('Should make an http POST to /orche/utilities and receive \'ping {ip}\'', async () => {
+    const result: string = await RequestHelper.post('/orche/utilities',
                                                     { ip: '192.168.0.21' });
     expect(result).to.be.equal('{"msg":"pinging 192.168.0.21"}');
   });
 
-  it('Should make an http PUT to /orche-decorators/utilities and receive \'ping {ip}\'', async () => {
-    const result: string = await RequestHelper.put('/orche-decorators/utilities',
-                                                  { ip: '192.168.0.21' });
+  it('Should make an http PUT to /orche/utilities and receive \'ping {ip}\'', async () => {
+    const result: string = await RequestHelper.put('/orche/utilities',
+                                                   { ip: '192.168.0.21' });
     expect(result).to.be.equal('{"msg":"pinging 192.168.0.21"}');
   });
 
-  it('Should make an http DELETE to /orche-decorators/utilities/{ip}', async () => {
-    const result: string = await RequestHelper.delete('/orche-decorators/utilities/192.168.0.21');
+  it('Should make an http DELETE to /orche/utilities/{ip}', async () => {
+    const result: string = await RequestHelper.delete('/orche/utilities/192.168.0.21');
     expect(result).to.be.equal('{"msg":"delete ip: 192.168.0.21"}');
   });
 
-  it('Should make an http PATCH to /orche-decorators/utilities and receive \'ping {ip}\'', async () => {
-    const result: string = await RequestHelper.patch('/orche-decorators/utilities',
-                                                    { ip: '192.168.0.21' });
+  it('Should make an http PATCH to /orche/utilities and receive \'ping {ip}\'', async () => {
+    const result: string = await RequestHelper.patch('/orche/utilities',
+                                                     { ip: '192.168.0.21' });
     expect(result).to.be.equal('{"msg":"pinging 192.168.0.21"}');
   });
 
-  it('Should make an http OPTIONS to /orche-decorators/utilities', async () => {
-    const result: string = await RequestHelper.options('/orche-decorators/utilities');
+  it('Should make an http OPTIONS to /orche/utilities', async () => {
+    const result: string = await RequestHelper.options('/orche/utilities');
     expect(result).to.be.equal('');
   });
 
-  it('Should make an http HEAD to /orche-decorators/utilities', async () => {
-    const result: string = await RequestHelper.head('/orche-decorators/utilities');
+  it('Should make an http HEAD to /orche/utilities', async () => {
+    const result: string = await RequestHelper.head('/orche/utilities');
     expect(result).to.be.equal('');
   });    
 });
--- test/decorators/interceptor.spec.ts
+++ test/decorators/interceptor.spec.ts
@@ -11,8 +11,36 @@ import {
 
 describe('Interceptor Decorators Tests', () => {
 
-  it('Should interceptor /orche-interceptors/utilities', () => {
-    
+  let result: OrcheResult;
+
+  before(async () => {
+    const orche = new Orche();
+
+    const config: OrcheConfig = {
+      path: '/orche',
+      apiEngine: OrcheEngines.ExpressJS,
+      port: 8888,
+      extensions: [
+        json(),
+      ]};
+
+    result = await orche.init(config);
+  });
+
+  after((done) => {
+    if (result.server) {
+      result.server.close(() => {
+        done();
+      });
+    }
+  });
+
+  it('Should initialize pre processing interceptors', () => {
+    expect(result.stats.interceptorStats.loadedPreProcessingInterceptors.length).to.be.gt(0);
+  });
+
+  it('Should initialize post processing interceptors', () => {
+    expect(result.stats.interceptorStats.loadedPostProcessingInterceptors.length).to.be.gt(0);
   });
 
 });
