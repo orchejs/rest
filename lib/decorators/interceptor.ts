84bf2acfc91782e285ab973f85f7735db267d4bc
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-07-25 01:45:16 -0300

    feat(decorator): interceptor attr must accept simple string besides array.

--- lib/decorators/interceptor.ts
+++ lib/decorators/interceptor.ts
@@ -2,22 +2,31 @@ import { InterceptorLoader } from '../loaders/interceptor.loader';
 import { InterceptorType } from '../constants/interceptor-type';
 
 
-export function interceptor(paths: string[] = ['/'], order?: number) {
+export function interceptor(paths: string | string[] = ['/'], order?: number) {
   return function (target: any) {
-    InterceptorLoader.addInterceptorConfig(paths, order);
+    const className: any = target.toString().match(/(function|class) ([^{(]*)/i);
+    let pathsArr: string[] = [];
+    if (!Array.isArray(paths)) {
+      pathsArr.push(paths);
+    } else {
+      pathsArr = paths;
+    }
+    InterceptorLoader.addInterceptorConfig(pathsArr, order, className);
   };
 }
 
 export function preProcessing() {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
     InterceptorLoader.addInterceptorUnit(descriptor.value.bind(target),
-                                         InterceptorType.PreProcessing);
+                                         InterceptorType.PreProcessing,
+                                         propertyKey);
   };
 }
 
 export function postProcessing() {
   return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
     InterceptorLoader.addInterceptorUnit(descriptor.value.bind(target),
-                                         InterceptorType.PostProcessing);
+                                         InterceptorType.PostProcessing,
+                                         propertyKey);
   };
 }
