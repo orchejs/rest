2ad49d044d89dba36532c9861a57014a3c255df8
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-07-26 01:27:21 -0300

    refactor(decorator): changed place to get the classname from array

--- lib/decorators/path.ts
+++ lib/decorators/path.ts
@@ -3,7 +3,7 @@ import { RouterLoader } from '../loaders/router.loader';
 
 export function path(path: string) {
   return function (target: Object) {
-    const className: any = target.toString().match(/(function|class) ([^{(]*)/i);
-    RouterLoader.addRouterConfig(className[2].trim(), path);
+    const className: any = target.toString().match(/(function|class) ([^{(]*)/i)[2].trim();
+    RouterLoader.addRouterConfig(className, path);
   };
 }
