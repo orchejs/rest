0578c052b023015e102d6a91afd6e34050f0598d
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-07-26 01:26:12 -0300

    feat(decorators): added http methods to better filter interceptors

--- lib/decorators/interceptor.ts
+++ lib/decorators/interceptor.ts
@@ -1,17 +1,29 @@
 import { InterceptorLoader } from '../loaders/interceptor.loader';
 import { InterceptorType } from '../constants/interceptor-type';
+import { HttpRequestMethod } from '../constants/http-request-method';
 
 
-export function interceptor(paths: string | string[] = ['/'], order?: number) {
+export function interceptor(paths: string | string[] = ['/'], 
+                            httpMethods: HttpRequestMethod | HttpRequestMethod[] 
+                            = [HttpRequestMethod.All], 
+                            order?: number) {
   return function (target: any) {
-    const className: any = target.toString().match(/(function|class) ([^{(]*)/i);
+    const className: any = target.toString().match(/(function|class) ([^{(]*)/i)[2].trim();
+
     let pathsArr: string[] = [];
     if (!Array.isArray(paths)) {
       pathsArr.push(paths);
     } else {
       pathsArr = paths;
     }
-    InterceptorLoader.addInterceptorConfig(pathsArr, order, className);
+
+    let httpMethodsArr: HttpRequestMethod[] = [];
+    if (!Array.isArray(httpMethods)) {
+      httpMethodsArr.push(httpMethods);
+    } else {
+      httpMethodsArr = httpMethods;
+    }
+    InterceptorLoader.addInterceptorConfig(pathsArr, order, className, httpMethodsArr);
   };
 }
 
