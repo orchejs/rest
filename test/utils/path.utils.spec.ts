84a797a91e855fa8acae4e41521b6498d6d155a9
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-08-08 07:56:29 -0300

    tests(utils): implementing path.utils test cases

---
+++ test/utils/path.utils.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { PathUtils } from '../../lib/utils/path.utils';
+
+describe('PathUtils', () => {
+  
+  describe('#urlSanitation', () => {
+    it('', () => {
+      PathUtils.urlSanitation();
+
+    });
+  });
+});
