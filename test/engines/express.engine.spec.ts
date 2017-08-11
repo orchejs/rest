30d1b77149c48a886316cb864969f006558d625b
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-08-11 07:56:57 -0300

    test(engine): finished express engine test cases

--- test/engines/express.engine.spec.ts
+++ test/engines/express.engine.spec.ts
@@ -1,21 +1,30 @@
+import { CorsOptions } from '../../lib/interfaces/cors-options';
+import { OrcheConfig } from '../..';
 import { expect } from 'chai';
 import { ExpressEngine } from '../../lib/engines/express.engine';
 
 describe('ExpressEngine', () => {
-  let exEngine: ExpressEngine;
-
-  before(() => {
-    exEngine = new ExpressEngine();    
-  });
 
   describe('#loadServer', () => {
     it('Should initialize the express app with the default options', async () => {
-      try {
-        const server = await exEngine.loadServer();
-        expect(server).to.not.undefined;
-      } catch (error) {
-        expect(error).to.undefined;
-      }
+      const exEngine: ExpressEngine = new ExpressEngine();        
+      const results = await exEngine.loadServer();
+
+      expect(results).to.not.undefined;
+
+      results.server.close();
+    });
+
+    it('Should initialize the express app with cors and production ready', async () => {
+      const orcheCfg: OrcheConfig = {
+        settings: {
+          env: 'production' },
+        corsConfig: { methods: '*' } };
+
+      const exEngine: ExpressEngine = new ExpressEngine(orcheCfg);
+      const results = await exEngine.loadServer();
+
+      expect(results).to.not.undefined;
     });
   });
 });
