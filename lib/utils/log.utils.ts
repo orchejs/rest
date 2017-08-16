dc813691d2e6d55e996e7b6da45d354340ff33a9
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-08-16 07:56:32 -0300

    feat(utils): log implementation with winston

--- lib/utils/log.utils.ts
+++ lib/utils/log.utils.ts
@@ -1,7 +1,31 @@
+import * as winston from 'winston';
+import * as moment from 'moment';
 
+import { PathUtils } from './path.utils';
 
+class LogUtils {
 
-export class LogUtils {
+  private static log: any;
+  public filename: string;
+
+  private constructor() {
+    if (!this.filename) {
+      this.filename = `${PathUtils.appDirName}-${moment().format('YYYYMMDDHHmmss')}.log`;
+    }
+
+    winston.configure({
+      transports:[
+        new (winston.transports.Console)({
+          colorize: true,
+        }),
+        new (winston.transports.File)({ filename: this.filename }),
+      ],
+    });
+  }
+
+  static setLogFileName(fileName: string) {
+
+  }
 
   static info(msg: string) {
 
@@ -20,3 +44,5 @@ export class LogUtils {
   }
 
 }
+
+export { LogUtils as log };
