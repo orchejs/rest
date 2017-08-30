83baeddab7f25ee70153dce7d5f6c296bc3a8cfd
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-08-30 07:47:15 -0300

    feat(decorators): validator decorator implementation

--- lib/decorators/validator.ts
+++ lib/decorators/validator.ts
@@ -1,5 +1,12 @@
-export function validate(validator: any) {
-  return function (teste: Symbol, target: any, propertyKey: string) {
-    
-  };
+import 'reflect-metadata';
+import { ValidatorDetails } from '../interfaces/validator-details';
+
+const validateMetadataKey = Symbol('validate');
+
+export function validate(validator: ValidatorDetails) {
+  return Reflect.metadata(validateMetadataKey, validator);
+}
+
+export function validators(target: any, propertyKey: string) {
+  return Reflect.getMetadata(validateMetadataKey, target, propertyKey);
 }
