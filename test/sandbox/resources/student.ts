9141bad6d294a8837bf18290525ce4516feda6f3
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-08-30 07:51:38 -0300

    test: created new class to test the validate decorator

--- test/sandbox/resources/student.ts
+++ test/sandbox/resources/student.ts
@@ -1,6 +1,19 @@
-import { validate } from '../../..';
+import { validate, NullValidator } from '../../..';
 
-export interface student {
-  @validate()
-  name: string;
+export class Student {
+  @validate({
+    validator: NullValidator
+  })
+  private _name: string;
+
+  constructor(name?: string) {
+    this.name = name;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+  set name(name: string) {
+    this.name = name;
+  }
 }
