32322e480ebd7a8fb9a7b0d556c96b900511f00d
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-10-06 00:33:51 -0300

    test(decorators): finished decorators tests - parameter decorators - route decorator

--- test/decorators/parameter.spec.ts
+++ test/decorators/parameter.spec.ts
@@ -20,6 +20,7 @@ import {
   QueryParam,
   PathParam,
   RequestParamMapper,
+  ExpressRequestMapper,
   BodyParam,
   HeaderParam,
   GenericResponse,
@@ -41,18 +42,23 @@ export class ComputersRs {
   @All('*')
   checkAccess(@RequestParam() req: Request) {
     const token: string = req.headers['authentication'];
-    req.headers['bearer'] = token;
+    req.query['bearer'] = token;
   }
 
   @Get(':uuid')
-  read(@PathParam('uuid') uuid: number, @HeaderParam('bearer') token: string) {
-    return new GenericResponse({ uuid, token }, HttpResponseCode.Ok);
+  read(@PathParam('uuid') uuid: number, @RequestParamMapper() mapper: ExpressRequestMapper) {
+    return new GenericResponse({ uuid, bearer: mapper.bearer }, HttpResponseCode.Ok);
   }
 
   @Get()
   list(@QueryParam('name') name: string, @QueryParam('size') size: number): GenericResponse {
     return new GenericResponse({ name, size }, HttpResponseCode.Ok);
-  }  
+  }
+
+  @Post()
+  create(@BodyParam() computer: Computer, @ResponseParam() res: Response) {
+    res.status(HttpResponseCode.Created).send(computer);
+  }
 }
 
 describe('Parameter decorators tests', () => {
@@ -62,22 +68,38 @@ describe('Parameter decorators tests', () => {
     expect(response.size).to.be.equal(10);
   });  
 
-  it('Should get token - bearer from HeaderParam', async () => {
-    const response = await RequestHelper.get(
-      '/orche/computers/1234', 
-      undefined, 
-      undefined, 
-      undefined, 
-      undefined, 
-      { 
-        authentication: '1234567' 
-      }
-    );
-    expect(response.token).to.be.equal('1234567');
-  });
+  it(
+    `Should get token - bearer from Request and set bearer as query param in RequestParamMapper`, 
+    async () => {
+      const response = await RequestHelper.get(
+        '/orche/computers/1234', 
+        undefined, 
+        undefined, 
+        undefined, 
+        undefined, 
+        { 
+          authentication: '1234567' 
+        }
+      );
+      expect(response.bearer).to.be.equal('1234567');
+    });
 
   it('Should get value from PathParam and return it', async () => {
     const response = await RequestHelper.get('/orche/computers/123');
     expect(response.uuid).to.be.equal(123);
-  });    
+  });
+
+  it(
+    'Should load computer object in BodyParam and return it using the ResponseParam', 
+    async () => {
+      const response = await RequestHelper.post(
+        '/orche/computers', 
+        { 
+          uuid: '123', 
+          model: 'Intel Core i5, SSD 500GB, 8 RAM'
+        }
+      );
+      expect(response.uuid).to.be.equal('123');
+      expect(response.model).to.be.equal('Intel Core i5, SSD 500GB, 8 RAM');
+    });
 });
---
+++ test/decorators/route.spec.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import { Route, Get } from '../..';
+import { RequestHelper } from '../helpers';
+
+export class ClassicalMusic {
+  uuid: string;
+  name: string;
+
+  constructor(uuid: string, name: string) {
+    this.uuid = uuid;
+    this.name = name;
+  }
+}
+
+@Route()
+export class ClassicalMusics {
+  @Get()
+  list(): ClassicalMusic[] {
+    return [
+      new ClassicalMusic('123', 'The marriage of Figaro')
+    ];
+  }
+}
+
+describe('Route decorator tests', () => {
+  it(
+    'Should create an endpoint with Route, using the classname as path, transformed to spinal-case',
+    async () => {
+      const result = await RequestHelper.get('/orche/classical-musics');
+      expect(result).to.be.not.undefined;
+    });
+});
