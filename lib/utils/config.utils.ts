0e03358a6c4f86fa38a3a4fdf2aa2039cf7e16ce
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-10-07 01:03:31 -0300

    refactor(utils): improved config loading. Added PORT and env from process.ENV

--- lib/utils/config.utils.ts
+++ lib/utils/config.utils.ts
@@ -8,7 +8,7 @@
 import { EventEmitter } from 'events';
 import * as fs from 'fs';
 
-import { PathUtils, UrlUtils, logger } from '@orchejs/common';
+import { Environment, logger, PathUtils, UrlUtils } from '@orchejs/common';
 import { OrcheRestConfig } from '../interfaces';
 import { OrcheEngine } from '../constants';
 
@@ -17,45 +17,56 @@ export class ConfigUtils extends EventEmitter {
 
   constructor() {
     super();
-    this.setMaxListeners(50);
+    this.setMaxListeners(0);
     this.loadOrcheConfig();
   }
 
   /**
-   * Priority hierarchy:
-   * 1 - User configs
-   * 2 - System variable ORCHE_CONFIG
-   * 3 - App .orcherc
-   * load .orcherc local or SYSTEM VARIABLE
+   * Config's merge, following this priority:
+   * - process.env variables
+   * - ENV's orche file configuration
+   * - LOCAL's orche file configuration
+   * - code orche config
    */
   loadOrcheConfig(userConfig: OrcheRestConfig = {}): void {
     this.config = {};
-    const configAppFileName = userConfig.appName || PathUtils.appDirName;
 
+    // App name used in .orcherc configuration files
+    const configAppName = userConfig.appName || PathUtils.appDirName;
     // Loads the environment orche config file contents
-    const envCfg: OrcheRestConfig = this.loadEnvConfigFile(configAppFileName);
+    const envCfg: OrcheRestConfig = this.loadEnvConfigFile(configAppName);
     // Load's the local orche config file contents
     const localCfg: OrcheRestConfig = this.loadLocalConfigFile();
 
-    /*
-     * Config's merge, following this priority:
-     * 1 - ENV's orche file configuration
-     * 2 - LOCAL's orche file configuration
-     * 3 - code orche config
-     */
+    // Config merge init
+    // API Engine - default Express
     this.config.apiEngine =
       envCfg.apiEngine || localCfg.apiEngine || userConfig.apiEngine || OrcheEngine.ExpressJS;
-
+    // Application main Path - default '/'
     const path = envCfg.path || localCfg.path || userConfig.path;
     this.config.path = UrlUtils.urlSanitation(path);
-
-    this.config.port = envCfg.port || localCfg.port || userConfig.port || 3000;
-    this.config.appName =
-      envCfg.appName || localCfg.appName || userConfig.appName || PathUtils.appDirName;
+    // Port - default 3000
+    this.config.port = process.env.PORT || envCfg.port || localCfg.port || userConfig.port || 3000;
+    // Log options - default undefined
+    this.config.logOptions = envCfg.logOptions || localCfg.logOptions || userConfig.logOptions;
+    // CORS config - default undefined
     this.config.corsConfig = envCfg.corsConfig || localCfg.corsConfig || userConfig.corsConfig;
+    // Debug mode - default false
     this.config.debug = envCfg.debug || localCfg.debug || userConfig.debug || false;
-    this.config.middlewares = envCfg.middlewares || localCfg.middlewares || userConfig.middlewares;
+    // Settings - default undefined
     this.config.settings = envCfg.settings || localCfg.settings || userConfig.settings;
+    // Base directory - default undefined
+    this.config.baseDir = envCfg.baseDir || localCfg.baseDir || userConfig.baseDir;
+    // Application name - default app directory name
+    this.config.appName = envCfg.appName || localCfg.appName || 
+                          userConfig.appName || PathUtils.appDirName;
+    // Middlewares - default []
+    this.config.middlewares = envCfg.middlewares || localCfg.middlewares || 
+                              userConfig.middlewares || [];
+    // Environment - default development
+    this.config.environment = process.env.ENV || envCfg.environment || 
+                              localCfg.environment || userConfig.environment || 
+                              'development';
 
     this.emit('configLoaded', this.config);
   }
@@ -71,16 +82,15 @@ export class ConfigUtils extends EventEmitter {
           if (fileContent) {
             fileContent = JSON.parse(fileContent);
             envCfg = fileContent['apps'] ? fileContent['apps'][configAppFileName] : {};
-            logger.debug(`Orche's environment config file loaded. 
-              File: ${process.env.ORCHE_CONFIG}`);
+            logger.debug(`Environment config file loaded. File: ${process.env.ORCHE_CONFIG}`);
           }
         } catch (error) {
-          throw new Error(`Orche's environment config file could not be loaded. Error: 
-            ${error.stack}`);
+          throw new Error(`Orche's environment config file could not be loaded. 
+                           Error: ${error.stack}`);
         }
       } else {
         throw new Error(`Orche's environment config file not found. File: 
-          ${process.env.ORCHE_CONFIG}`);
+                         ${process.env.ORCHE_CONFIG}`);
       }
     }
     return envCfg;
@@ -94,8 +104,7 @@ export class ConfigUtils extends EventEmitter {
         const fileContent = fs.readFileSync(PathUtils.localConfigFile, 'utf8');
         if (fileContent) {
           localCfg = JSON.parse(fileContent);
-          logger.debug(`Orche's local config file loaded. 
-            File: ${process.env.ORCHE_CONFIG}`);
+          logger.debug('Local config file loaded');
         }
       } catch (error) {
         throw new Error(`Orche's local config file could not be loaded. Error: ${error.stack}`);
