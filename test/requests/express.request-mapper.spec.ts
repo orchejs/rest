411ca0244c5942c4ebd49acde64e2991db54a04d
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-10-11 00:17:36 -0300

    test(requests): ended test cases for express request-mapper

---
+++ test/requests/express.request-mapper.spec.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright Mauricio Gemelli Vigolo. All Rights Reserved.
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file at https://github.com/orchejs/rest/LICENSE
+ */
+import { expect } from 'chai';
+import { Request } from 'express';
+import { ExpressRequestMapper } from '../..';
+
+describe('ExpressRequestMapper', () => {
+  describe('#loadPathParams and #loadQueryParams', () => {
+    it('Should initialize all path and query params from express.Request', () => {
+      const req: Request = {} as Request;
+      req.params = { uuid: '123' };
+      req.query = { size: '10' };
+      const erm = new ExpressRequestMapper(req);
+      expect(erm.size).to.be.eq('10');
+      expect(erm.uuid).to.be.eq('123');
+    });
+  });
+});
