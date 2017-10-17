61fc080d846f32b66ea560852be60314677e509d
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-10-16 23:37:39 -0200

    test(routers): started to create express router test cases

---
+++ test/routers/express.router.spec.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Mauricio Gemelli Vigolo.
+ *
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file at https://github.com/orchejs/rest/LICENSE
+ */
+import { expect } from 'chai';
+
+describe('ExpressRouter', () => {
+  describe('#loadRouters', () => {
+    it('', () => {
+    });
+  });
+});
